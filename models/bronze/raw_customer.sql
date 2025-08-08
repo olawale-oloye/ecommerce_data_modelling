152fd2fec20acd6cc8ce9a982d6f17edb8d72962
Ola Ola <olaolibale@gmail.com>
2025-08-08 08:04:50 +0000

    commit

--- models/bronze/raw_customer.sql
+++ models/bronze/raw_customer.sql
@@ -1,4 +1,7 @@
 with customers_table as (
-    SELECT * FROM {{source('ecommerce_data_modelling','customers')}}
+    -- SELECT * FROM {{source('ecommerce_data_modelling','customers')}}
+    SELECT * FROM ECOMMERCE_DWH.ECOMMERCE_STAGING.customers
 ) 
 SELECT count(id) FROM customers_table
+
+ 
